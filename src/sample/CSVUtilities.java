ffd686e99521a13642521133d8e5887ca2fa9371
BT_1N3_03@BKTECH.LOCAL <BT_1N3_03@BKTECH.LOCAL>
2018-12-12 09:48:24 -0500

    Initial commit

---
+++ src/sample/CSVUtilities.java
@@ -0,0 +1,12 @@
+package sample;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class CSVUtilities {
+    ArrayList<String> CVSData = new ArrayList<>();
+    int numColumns;
+    public CSVUtilities(File csv){
+        
+    }
+}
